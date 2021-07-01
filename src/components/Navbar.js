5884f25b815dff98e3c2c48ef01406ef99475d6b
anishasbhat <anishasbhat@gmail.com>
2021-07-01 01:41:40 -0700

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,35 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
+import './Navbar.css';
+import { Button } from './Button';
+
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true)
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    }
+
+    useEffect(() => {
+        showButton()
+    }, [])
+
+    window.addEventListener('resize', showButton);
     return (
         <>
         <nav className='navbar'>
             <div className='navbar-container'>
-                <Link to="/" className='navbar-logo'>
-                    TRVL <i className='fab fa-typo3'></i >
+                <Link to="/" className='navbar-logo' onClick= 
+                {closeMobileMenu}>
+                    AB <i className='fab fa-typo3'></i >
+                    <div comment="fab fa-typo3 can del"> </div>
                 </Link>
                 <div className='menu-icon' onClick={handleClick}>
                     <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
@@ -39,6 +59,7 @@ function Navbar() {
                         </Link>
                     </li>
                 </ul>
+                {button && <Button buttonStyle='btn--outline'>Sign Up</Button>}
             </div>
         </nav> 
         </>
