4b3ef15f6de3ecca91ea6bd85b87ccf5f9712a90
anishasbhat <anishasbhat@gmail.com>
2021-07-27 00:34:30 -0700

    v2

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -8,19 +8,20 @@ function Home() {
     return (
         <>
         {/*<h1 class="font"> ANISHA BHAT </h1> */}
-        <img src={profile} class="face"></img>
+        {/*<img src={profile} class="face"></img>*/}
         <p class="home-text">
-        <h1 class="font"> ANISHA BHAT </h1>
+        <h1 class="title"> Hi, I'm Anisha! </h1>
 
-        <p class="subtext"> Computer Science Undergraduate at Purdue University</p>
-      <br></br><br></br>
-      <p class="para">I'm a junior studying CS at Purdue University, with a minor in Management and a concentration in ML/AI. 
-      At Purdue, I lead the UX/Logistics team of Purdue's largest hackathon, Boilermake. 
-      Currently, I serve on the Purdue CS Undergraduate Student Board and provide guidance as a mentor at Launchpad, Purdue's
+        <p class="subtext"> I'm a junior studying CS at Purdue University, with a minor in Management and a concentration in ML/AI.</p>
+      <br></br>
+      <p class="para"> 
+      At Purdue, I lead the UX/Logistics team of Purdue's largest hackathon, Boilermake. <br></br>
+      Currently, I serve on the Purdue CS Undergraduate Student Board and provide <br></br> guidance as a mentor at Launchpad, Purdue's
       freshmen technical project program. </p>
       <br></br> 
-      <p class="para">In my free time, I like to cook new foods, and watch sunsets. This website is currently still under construction... stay tuned for updates! 🦋</p>
+      <p class="para">In my free time, I like to cook new foods, and watch sunsets. </p>
       <br></br>
+      <p class="subtext"> This website is currently still under construction... more coming soon! 🦋 </p>
       </p>
         </>
     );
